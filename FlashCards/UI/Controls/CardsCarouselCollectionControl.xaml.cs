179d3f19a0ccc40c8f0612305f2a75f2f1f0250a
MOZU <mozu@demant.com>
2024-06-20 23:27:30 +0200

    Revert "Animation of Carousel in progress - doesn't work yet"
    This reverts commit 7e6f2f81a4905b61515a21e6ce854e61bd9efae3.

--- FlashCards/UI/Controls/CardsCarouselCollectionControl.xaml.cs
+++ FlashCards/UI/Controls/CardsCarouselCollectionControl.xaml.cs
@@ -5,60 +5,14 @@ namespace FlashCards.UI.Controls;
 
 public partial class CardsCarouselCollectionControl : ContentView
 {
+    private const double SwipeThreshold = 50;
+
     [BindableProperty(typeof(List<SingleCard>), OnPropertyChanged = nameof(OnCardsCollectionPropertyChanged))]
     public static readonly BindableProperty CardsCollectionProperty;
 
     public CardsCarouselCollectionControl()
     {
         InitializeComponent();
-        Carousel.CurrentItemChanged += CarouselOnCurrentItemChanged;
-    }
-
-    private void CarouselOnCurrentItemChanged(object? sender, CurrentItemChangedEventArgs e)
-    {
-        if (e.PreviousItem != null)
-        {
-            var previousCardBorder = GetBorderFromDataContext(e.PreviousItem);
-            if (previousCardBorder != null)
-            {
-                Animate(previousCardBorder);
-            }
-        }
-
-        if (e.CurrentItem != null)
-        {
-            var currentCardBorder = GetBorderFromDataContext(e.CurrentItem);
-            if (currentCardBorder != null)
-            {
-                Animate(currentCardBorder);
-            }
-        } 
-    }
-
-    private Border? GetBorderFromDataContext(object dataContext)
-    {
-        var currentElement =  ((List<SingleCard>)dataContext).FirstOrDefault(x => x == dataContext);
-        if (currentElement != null)
-        {
-            return currentElement as Border;
-        }
-        // foreach (var item in Carousel.ItemsSource)
-        // {
-        //     var visualElement = Carousel.ItemTemplate.CreateContent() as View;
-        //     // if (visualElement?.BindingContext == dataContext)
-        //     if ((SingleCard)visualElement?.BindingContext! == dataContext)
-        //     {
-        //         return visualElement as Border;
-        //     }
-        // }
-        return null;
-    }
-    
-    private async void Animate(Border border)
-    {
-        await border.RotateYTo(90, 250);
-        border.RotationY = -90;
-        await border.RotateYTo(0, 250);
     }
     
     private static void OnCardsCollectionPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
