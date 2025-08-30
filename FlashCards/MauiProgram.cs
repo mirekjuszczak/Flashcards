bbf108c0fef362d9510195deb6653e27c420e35f
MOZU <mozu@demant.com>
2025-08-30 19:06:17 +0200

    packages in csproj only and builds hmm.... -Firebase, Firestore wrong versions

--- FlashCards/MauiProgram.cs
+++ FlashCards/MauiProgram.cs
@@ -1,68 +1,58 @@
 ﻿using FlashCards.Services.DatabaseService;
-using FlashCards.Services.DatabaseServiceMock;
 using FlashCards.Services.FirebaseService;
 using FlashCards.Services.Navigation;
 using FlashCards.Showroom;
 using FlashCards.ViewModels;
 using FlashCards.Views;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Debug;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using CommunityToolkit.Maui;
 
 namespace FlashCards;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("Font-Regular.ttf");
-				fonts.AddFont("Font-SemiBold.ttf");
-				fonts.AddFont("Font-Bold.ttf");
-			});
-		
-		RegisterViewModelsAndPages(builder);
-		RegisterServices(builder);
-
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("Font-Regular.ttf");
+            fonts.AddFont("Font-SemiBold.ttf");
+            fonts.AddFont("Font-Bold.ttf");
+        }).UseMauiCommunityToolkit();
+        RegisterViewModelsAndPages(builder);
+        RegisterServices(builder);
 #if DEBUG
-		builder.Logging.SetMinimumLevel(LogLevel.Debug);
+        builder.Logging.AddDebug();
 #endif
+        return builder.Build();
+    }
 
-		return builder.Build();
-	}
-	
-	private static void RegisterViewModelsAndPages(MauiAppBuilder builder)
-	{
-		builder.Services.AddSingleton<StartPage>();
-		builder.Services.AddSingleton<StartPageViewModel>();
-		
-		builder.Services.AddSingleton<ShowroomPage>();
-		builder.Services.AddSingleton<ShowroomPageViewModel>();
-		
-		builder.Services.AddSingleton<TwoSidesCardControlPage>();
-		builder.Services.AddSingleton<TwoSidesCardControlPageViewModel>();
-		
-		builder.Services.AddSingleton<CardsSwipeCollectionControlPage>();
-		builder.Services.AddSingleton<CardsCarouselCollectionPage>();
-		builder.Services.AddSingleton<CardsCollectionControlPageViewModel>();
-		builder.Services.AddSingleton<CardsCollection3Page>();
-		builder.Services.AddSingleton<CardsCollection3ViewModel>();
-		
-		builder.Services.AddSingleton<CategoryCardPage>();
-		builder.Services.AddSingleton<CategoriesCollectionPage>();
-		builder.Services.AddSingleton<CategoriesPageViewModel>();
-	}
+    private static void RegisterViewModelsAndPages(MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<StartPage>();
+        builder.Services.AddSingleton<StartPageViewModel>();
+        builder.Services.AddSingleton<ShowroomPage>();
+        builder.Services.AddSingleton<ShowroomPageViewModel>();
+        builder.Services.AddSingleton<TwoSidesCardControlPage>();
+        builder.Services.AddSingleton<TwoSidesCardControlPageViewModel>();
+        builder.Services.AddSingleton<CardsSwipeCollectionControlPage>();
+        builder.Services.AddSingleton<CardsCarouselCollectionPage>();
+        builder.Services.AddSingleton<CardsCollectionControlPageViewModel>();
+        builder.Services.AddSingleton<CardsCollection3Page>();
+        builder.Services.AddSingleton<CardsCollection3ViewModel>();
+        builder.Services.AddSingleton<CategoryCardPage>();
+        builder.Services.AddSingleton<CategoriesCollectionPage>();
+        builder.Services.AddSingleton<CategoriesPageViewModel>();
+    }
 
-	private static void RegisterServices(MauiAppBuilder builder)
-	{
-		builder.Services.AddSingleton<INavigationService, NavigationService>();
-		// builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
-		
-		builder.Services.AddSingleton<IDatabaseService, FirebaseDatabaseService>();
-		builder.Services.AddSingleton<IUserService, FirebaseAuthService>();
-	}
+    private static void RegisterServices(MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<INavigationService, NavigationService>();
+        // builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
+        builder.Services.AddSingleton<IDatabaseService, FirebaseDatabaseService>();
+        builder.Services.AddSingleton<IUserService, FirebaseAuthService>();
+    }
 }
