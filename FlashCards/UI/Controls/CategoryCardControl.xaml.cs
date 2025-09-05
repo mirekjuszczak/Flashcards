ce5d4621eb7e0ab669a7dec3cba5deaa238b0953
MOZU <mozu@demant.com>
2025-09-05 12:48:49 +0200

    refactor

--- FlashCards/UI/Controls/CategoryCardControl.xaml.cs
+++ FlashCards/UI/Controls/CategoryCardControl.xaml.cs
@@ -1,13 +1,19 @@
 using Bindables.Maui;
-using Microsoft.Maui.Controls;
 
 namespace FlashCards.UI.Controls;
 
 public partial class CategoryCardControl : Border
 {
+    private static readonly int DefaultIntValue = 0;
+
     [BindableProperty(typeof(string), BindingMode = BindingMode.TwoWay)]
     public static readonly BindableProperty CategoryNameProperty;
-    
+
+    [BindableProperty(typeof(int), 
+        BindingMode = BindingMode.TwoWay,
+        DefaultValueField = nameof(DefaultIntValue))]
+    public static readonly BindableProperty CardsCountProperty;
+
     public CategoryCardControl()
     {
         InitializeComponent();
